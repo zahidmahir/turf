ff850ae107cb908424d37d3a0d368fbb36cd4816
DanielRings <rings.daniel@gmail.com>
2013-09-28 01:23:44 -0400

    rudimentary index.html/mapping.js works

--- front/web/static/js/mapping.js
+++ front/web/static/js/mapping.js
@@ -1,52 +1,64 @@
-console.log('im a faggot')
-var fromProjection = new OpenLayers.Projection("EPSG:4326");   // Transform from WGS 1984
-var toProjection = new OpenLayers.Projection("EPSG:900913"); // to Spherical Mercator Projection
-function transform(lon, lat) {
-    return new OpenLayers.LonLat(lon, lat).transform(fromProjection, toProjection);
+$(document).ready(function(){
+  var map = new OpenLayers.Map('map');
+  var layer = new OpenLayers.Layer.WMS( "OpenLayers WMS", "http://vmap0.tiles.osgeo.org/wms/vmap0", {layers: 'basic'} );
+  var redStyle = {
+    fill: true,
+    fillColor: '#FF0000',
+    fillOpacity: 0.4,
+    strokeColor: '#FF0000'
+  };
+  var greenStyle = {
+    fill: true,
+    fillColor: '#00FF22',
+    fillOpacity: 0.4,
+    strokeColor: '#00FF22'
+  };
+  var blueStyle = {
+    fill: true,
+    fillColor: '#0000FF',
+    fillOpacity: 0.4,
+    strokeColor: '#0000FF'
+  };
+  var yellowStyle = {
+    fill: true,
+    fillColor: '#FFFF00',
+    fillOpacity: 0.4,
+    strokeColor: '#FFFF00'
+  };
+  var emptyStyle = {
+    fill: false,
+    strokeColor: '#444444'
+  }
+  var cvRed = new OpenLayers.Layer.Vector();
+  var cvBlue = new OpenLayers.Layer.Vector();
+  var cvGreen = new OpenLayers.Layer.Vector();
+  var cvYellow = new OpenLayers.Layer.Vector();
+  var cvEmpty = new OpenLayers.Layer.Vector();
+  var territories;
+  map.addLayer(layer);
+  map.setCenter(new OpenLayers.LonLat(-73.95, 40.75), 12);
+  cvRed.style = redStyle;
+  cvBlue.style = blueStyle;
+  cvGreen.style = greenStyle;
+  cvYellow.style = yellowStyle;
+  cvEmpty.style = emptyStyle;
+
+  function init(){
+
+  }
+
+  function drawTerritory(t, cv){
+    console.log('t: ', t);
+    cv.addFeatures(geojson_format.read(t));
+    map.addLayer(cv);
+  }
+
+  // function getTerritories(){
+    
+  //   $.getJSON
+  //     res
+  //       foreach r in res
+  //         drawTerritory(r)
+  // }
 }
-
-var redStyle = {
-  fill: true,
-  fillColor: '#FF0000',
-  fillOpacity: 0.4,
-  strokeColor: '#FF0000'
-};
-var greenStyle = {
-  fill: true,
-  fillColor: '#00FF22',
-  fillOpacity: 0.4,
-  strokeColor: '#00FF22'
-};
-var blueStyle = {
-  fill: true,
-  fillColor: '#0000FF',
-  fillOpacity: 0.4,
-  strokeColor: '#0000FF'
-};
-var yellowStyle = {
-  fill: true,
-  fillColor: '#FFFF00',
-  fillOpacity: 0.4,
-  strokeColor: '#FFFF00'
-};
-var emptyStyle = {
-  fill: false,
-  strokeColor: '#444444'
-}
-
-var territories;
-
-function init(){
-
-}
-
-function drawTerritory(t){
-
-}
-
-function getTerritories(){
-  $.getJSON
-    res
-      foreach r in res
-        drawTerritory(r)
-}
+);
