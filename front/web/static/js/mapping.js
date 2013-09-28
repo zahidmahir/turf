e0b5006078a22d7ef83685f9def509732894677a
csakai <cs.chris.sakai@gmail.com>
2013-09-28 16:24:05 -0400

    re-added hiding HUD

--- front/web/static/js/mapping.js
+++ front/web/static/js/mapping.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    $("#hud").hide();
     var fromProjection = new OpenLayers.Projection("EPSG:4326");   // Transform from WGS 1984
     var toProjection = new OpenLayers.Projection("EPSG:900913"); // to Spherical Mercator Projection
 
