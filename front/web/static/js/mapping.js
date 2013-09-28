7cb2b1bba2301a2da7f6df4bb7bf39cf718b8837
DanielRings <rings.daniel@gmail.com>
2013-09-28 04:45:46 -0400

    improved front end

--- front/web/static/js/mapping.js
+++ front/web/static/js/mapping.js
@@ -1,4 +1,13 @@
 $(document).ready(function(){
+  //fix the effing screen
+  document.getElementById('map').style.height = (window.innerHeight)+"px";
+  document.getElementById('map').style.width = (window.innerWidth)+"px";
+  document.getElementById('hud').style.top = (window.innerHeight)*0.7;
+  var owner = 'Red Team';
+  var area = '10038';
+  document.getElementById('info').innerHTML = 'OWNER: ' + owner + ' AREA: ' + area;
+
+  //make all vars
   var map = new OpenLayers.Map('map');
   var layer = new OpenLayers.Layer.WMS( "OpenLayers WMS", "http://vmap0.tiles.osgeo.org/wms/vmap0", {layers: 'basic'} );
   var redStyle = {
@@ -35,6 +44,8 @@ $(document).ready(function(){
   var cvYellow = new OpenLayers.Layer.Vector();
   var cvEmpty = new OpenLayers.Layer.Vector();
   var territories;
+
+  //set values for vars
   map.addLayer(layer);
   map.setCenter(new OpenLayers.LonLat(-73.95, 40.75), 12);
   cvRed.style = redStyle;
@@ -43,22 +54,27 @@ $(document).ready(function(){
   cvYellow.style = yellowStyle;
   cvEmpty.style = emptyStyle;
 
-  function init(){
-
-  }
-
   function drawTerritory(t, cv){
     console.log('t: ', t);
     cv.addFeatures(geojson_format.read(t));
     map.addLayer(cv);
   }
 
-  // function getTerritories(){
-    
-  //   $.getJSON
-  //     res
-  //       foreach r in res
-  //         drawTerritory(r)
-  // }
+  function getTerritories(){
+    territories = $.getJSON("http://www.zmhr.me/geoAllGeo");
+  }
+
+  function init(){
+    territories.forEach(function(t){
+      if(t.owner = ''){
+        drawTerritory(t, cvEmpty);
+      }
+      else{
+        drawTerritory(t, cvRed);
+      }
+    });
+  }
+  getTerritories();
+  console.log(territories);
 }
 );
