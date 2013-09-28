2cfcec9c149dbd7010ed920cc8249b179d293361
Zahid Mahir <zahidmahir@gmail.com>
2013-09-28 13:33:58 -0400

    shits fucked

--- front/web/static/js/mapping.js
+++ front/web/static/js/mapping.js
@@ -1,6 +1,68 @@
 $(document).ready(function(){
+    var fromProjection = new OpenLayers.Projection("EPSG:4326");   // Transform from WGS 1984
+    var toProjection = new OpenLayers.Projection("EPSG:900913"); // to Spherical Mercator Projection
+
+    var my_style = {
+      fill: true,
+      fillColor: '#00aaff',
+      fillOpacity: 0.5,
+      strokeColor: '#333ff'
+    };
+    var enemy_style = {
+      fill: true,
+      fillColor: '#ff0000',
+      fillOpacity: 0.5,
+      strokeColor: '#cc1144'
+    };
+    var emptyStyle = {
+      fill: false,
+      strokeDashstyle: 'dash',
+      fillOpacity: 0.1
+    }
+
+    function transform(lon, lat) {
+      return new OpenLayers.LonLat(lon, lat).transform(fromProjection, toProjection);
+    }
+
+    // var map = new OpenLayers.Map('map');
+    // var layer = new OpenLayers.Layer.OSM();
+
+    var map = new OpenLayers.Map('map');
+    var layer = new OpenLayers.Layer.WMS( "OpenLayers WMS", "http://vmap0.tiles.osgeo.org/wms/vmap0", {layers: 'basic'} );
+
+    map.addLayer(layer);
+    // map.setCenter(transform(-95, 40), 4, false, false);
+
+    var geojson_format = new OpenLayers.Format.GeoJSON();
+
+    function featureToGeoJSON(feature) {
+      return geojson_format.read(feature);
+    }
+    // colored_vector.addFeatures(featureToGeoJSON(some_feature));
+
+    var my_territory_vector = new OpenLayers.Layer.Vector();
+    var enemy_territory_vector = new OpenLayers.Layer.Vector();
+
+    var empty_territory_vector = new OpenLayers.Layer.Vector();
+
+    map.addLayers([my_territory_vector, enemy_territory_vector, empty_territory_vector]);
+
+    my_territory_vector.style = my_style;
+    enemy_territory_vector.style = enemy_style;
+    empty_territory_vector.style = empty_territory_vector;
+
+    $.getJSON('http://zmhr.me/getAllGeo', function(res) {
+      JSON.parse(res).rows.forEach(function(r) {
+        console.log('adding', r.value);
+
+        //daniel make this shit work
+
+        enemy_territory_vector.addFeatures(featureToGeoJSON(r.value.geo_json));
+      });
+    });
+
   //fix the effing screen
-  document.getElementById('map').style.height = (window.innerHeight)+"px";
+/*  document.getElementById('map').style.height = (window.innerHeight)+"px";
   document.getElementById('map').style.width = (window.innerWidth)+"px";
   document.getElementById('hud').style.top = (window.innerHeight)*0.7;
   var owner = 'Red Team';
@@ -75,6 +137,5 @@ $(document).ready(function(){
     });
   }
   getTerritories();
-  console.log(territories);
-}
-);
+  console.log(territories);*/
+});
