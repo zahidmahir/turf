6e5e5d5eb7169c0cfa6daa1cd717822aff5575a7
csakai <cs.chris.sakai@gmail.com>
2013-09-28 17:14:13 -0400

    #Please enter the commit message for your changes. Lines starting

--- front/web/static/js/mapping.js
+++ front/web/static/js/mapping.js
@@ -1,17 +1,15 @@
-var lat=0;
-var lng=0;
 $(document).ready(function(){
   $("#hud").hide();
-  if (navigator.geolocation)
-  {
-    navigator.geolocation.getCurrentPosition(showPosition);
-  }
-  else{alert("Geolocation is not supported by this browser.");}
-  function showPosition(position)
-  {
-    lat=position.coords.latitude;
-    lng=position.coords.longitude; 
-  }
+  // if (navigator.geolocation)
+  // {
+  //   navigator.geolocation.getCurrentPosition(showPosition);
+  // }
+  // else{alert("Geolocation is not supported by this browser.");}
+  // function showPosition(position)
+  // {
+  //   lat=position.coords.latitude;
+  //   lng=position.coords.longitude; 
+  // }
     var fromProjection = new OpenLayers.Projection("EPSG:4326");   // Transform from WGS 1984
     var toProjection = new OpenLayers.Projection("EPSG:900913"); // to Spherical Mercator Projection
 
@@ -42,7 +40,7 @@ $(document).ready(function(){
     var layer = new OpenLayers.Layer.WMS( "OpenLayers WMS", "http://vmap0.tiles.osgeo.org/wms/vmap0", {layers: 'basic'} );
     map.addLayer(layer);
 
-    map.setCenter(new OpenLayers.LonLat(lng, lat), 4, false, false);
+    //map.setCenter(new OpenLayers.LonLat(lng, lat), 4, false, false);
 
     function transform(lon, lat) {
       return new OpenLayers.LonLat(lon, lat).transform(fromProjection, toProjection);
@@ -113,4 +111,119 @@ $(document).ready(function(){
     }
   });
 
+  var power = 1;
+  if (navigator.userAgent.toLowerCase().indexOf('windows') >= 0){
+    power = 2;
+  }
+  console.log(navigator.userAgent);
+  console.log(power);
+
+var style = {
+    fillColor: '#000',
+    fillOpacity: 0.1,
+    strokeWidth: 0
+};
+
+var lat= 0;
+var lon= 0;
+var vector = new OpenLayers.Layer.Vector('vector');
+map.addLayers([layer, vector]);
+
+map.setCenter(
+    new OpenLayers.LonLat(-71.147, 42.472).transform(
+        new OpenLayers.Projection("EPSG:4326"),
+        map.getProjectionObject()
+    ), 12
+);
+
+var pulsate = function(feature) {
+    var point = feature.geometry.getCentroid(),
+        bounds = feature.geometry.getBounds(),
+        radius = Math.abs((bounds.right - bounds.left)/2),
+        count = 0,
+        grow = 'up';
+
+    var resize = function(){
+        if (count>16) {
+            clearInterval(window.resizeInterval);
+        }
+        var interval = radius * 0.03;
+        var ratio = interval/radius;
+        switch(count) {
+            case 4:
+            case 12:
+                grow = 'down'; break;
+            case 8:
+                grow = 'up'; break;
+        }
+        if (grow!=='up') {
+            ratio = - Math.abs(ratio);
+        }
+        feature.geometry.resize(1+ratio, point);
+        vector.drawFeature(feature);
+        count++;
+    };
+    window.resizeInterval = window.setInterval(resize, 50, point, radius);
+};
+
+var geolocate = new OpenLayers.Control.Geolocate({
+    bind: false,
+    geolocationOptions: {
+        enableHighAccuracy: false,
+        maximumAge: 0,
+        timeout: 7000
+    }
 });
+map.addControl(geolocate);
+var firstGeolocation = true;
+geolocate.events.register("locationupdated",geolocate,function(e) {
+    vector.removeAllFeatures();
+    lat = e.point.x;
+    lon = e.point.y;
+    console.log(lat, lon);
+    var circle = new OpenLayers.Feature.Vector(
+        OpenLayers.Geometry.Polygon.createRegularPolygon(
+            new OpenLayers.Geometry.Point(e.point.x, e.point.y),
+            e.position.coords.accuracy/2,
+            40,
+            0
+        ),
+        {},
+        style
+    );
+    vector.addFeatures([
+        new OpenLayers.Feature.Vector(
+            e.point,
+            {},
+            {
+                graphicName: 'cross',
+                strokeColor: '#f00',
+                strokeWidth: 2,
+                fillOpacity: 0,
+                pointRadius: 10
+            }
+        ),
+        circle
+    ]);
+    if (firstGeolocation) {
+        map.setCenter(new OpenLayers.LonLat(lon, lat), 12, false, false);
+        pulsate(circle);
+        firstGeolocation = false;
+        this.bind = true;
+    }
+});
+geolocate.events.register("locationfailed",this,function() {
+    OpenLayers.Console.log('Location detection failed');
+});
+    vector.removeAllFeatures();
+    geolocate.deactivate();
+    geolocate.watch = false;
+    firstGeolocation = true;
+    geolocate.activate();
+
+    console.log(geolocate);
+
+});
+
+
+//getGeo/11355
