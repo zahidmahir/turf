b3ee6caa0f49ce86bddccef810c261bc90e65679
DanielRings <rings.daniel@gmail.com>
2013-09-28 00:48:38 -0400

    added jquery and openlayers res

--- front/web/static/js/mapping.js
+++ front/web/static/js/mapping.js
@@ -1,3 +1,6 @@
+console.log('im a faggot')
+var fromProjection = new OpenLayers.Projection("EPSG:4326");   // Transform from WGS 1984
+var toProjection = new OpenLayers.Projection("EPSG:900913"); // to Spherical Mercator Projection
 function transform(lon, lat) {
     return new OpenLayers.LonLat(lon, lat).transform(fromProjection, toProjection);
 }
@@ -26,3 +29,24 @@ var yellowStyle = {
   fillOpacity: 0.4,
   strokeColor: '#FFFF00'
 };
+var emptyStyle = {
+  fill: false,
+  strokeColor: '#444444'
+}
+
+var territories;
+
+function init(){
+
+}
+
+function drawTerritory(t){
+
+}
+
+function getTerritories(){
+  $.getJSON
+    res
+      foreach r in res
+        drawTerritory(r)
+}
